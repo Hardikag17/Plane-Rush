1d674a4fc2e8265f11a397936b765956235f9660
Hardikag17 <hardikag17@gmail.com>
2021-11-24 03:59:10 +0530

    minor changes

--- contracts/game.sol
+++ contracts/game.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Migrations {
+contract game {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const game = artifacts.require("game");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(game);
 };
