831f2ff8ddc43620ec9979cb6989a8b29fcb7268
Hardikag17 <hardikag17@gmail.com>
2021-11-28 04:42:36 +0530

    github repo link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 function Footer() {
   return (
-    <footer className='text-center absolute inset-x-0 bottom-0  px-2 py-3 bg-Primary text-white '>
+    <div className='absolute inset-x-0 text-center mx-auto bg-Primary text-gray-300 p-2 bottom-0 '>
       <h1>Made with 💙 by DTech</h1>
-    </footer>
+      <a
+        href='https://github.com/Hardikag17/DTech'
+        src=''
+        className=' text-white text-xl hover:text-gray-300 absolute right-1 bottom-2 '>
+        <i class='fab fa-github'></i>
+      </a>
+    </div>
   );
 }
 
