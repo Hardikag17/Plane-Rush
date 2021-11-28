78fdceedd4fe5d7007d94f735667e399f09b358d
Dinesh B S <dineshbs444@gmail.com>
2021-11-28 19:44:37 +0530

    modified contract and migration

--- migrations/1_nft_character.js
+++
@@ -1,6 +0,0 @@
-const NFTCharacter = artifacts.require("NFTCharacter");
-
-module.exports = async (deployer) => {
-  await deployer.deploy(NFTCharacter);
-  let nftCharacter = await NFTCharacter.deployed();
-};
